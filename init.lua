031c242b24bed7b5c6b7c2b780c0bd453d8e949a
azekillDIABLO <Rodrigue.Lemaire@gmail.com>
2016-04-24 16:32:09 +0200

    Added files via upload

---
+++ init.lua
@@ -0,0 +1,7 @@
+local path = minetest.get_modpath("azewheel")
+dofile(path.."/bus.lua")
+dofile(path.."/car.lua")
+dofile(path.."/bike.lua")
+dofile(path.."/neonbike.lua")
+
+print("Have fun with your motorized vehicules!!~~~~By azekill_DIABLO~~~~")
